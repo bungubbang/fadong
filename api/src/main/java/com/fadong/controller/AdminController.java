f0ca1e755d2716032a07328a37d20bf4ed846862
정성용 <sungyong.jung@sk.com>
2017-01-24 11:36:24 +0900

    vue 로 admin 페이지 만듬

--- api/src/main/java/com/fadong/controller/AdminController.java
+++ api/src/main/java/com/fadong/controller/AdminController.java
@@ -7,10 +7,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * Created by 1000742 on 2017. 1. 2..
  */
 @Controller
+@RequestMapping("admin")
 public class AdminController {
 
-    @RequestMapping("admin")
-    private String admin() {
-        return "admin";
+    @RequestMapping("page")
+    private String page() {
+        return "admin/page";
+    }
+
+    @RequestMapping("card")
+    private String card() {
+        return "admin/card";
     }
 }
