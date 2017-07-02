64f87ef97e8171dc7fd7e5ffb153326af98403a5
bungubbang <bungubbang57@gmail.com>
2017-07-02 22:59:40 +0900

    미세먼지 테스트 추가

---
+++ api/src/test/java/com/fadong/bot/service/NowWeather.java
@@ -0,0 +1,40 @@
+package com.fadong.bot.service;
+
+import org.junit.Test;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+import org.springframework.web.util.UriTemplate;
+
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by bungubbang on 02/07/2017.
+ */
+public class NowWeather {
+
+    @Test
+    public void now() {
+        String url = "http://apis.skplanetx.com/weather/airquality/current?version={version}&lat={lat}&lon={lon}";
+        UriTemplate uriTemplate = new UriTemplate(url);
+        Map<String, String> param = new HashMap<>();
+        param.put("version", "1");
+        param.put("lat", "37.1901066896");
+        param.put("lon", "127.0866092541");
+        URI uri = uriTemplate.expand(param);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("appKey", "ad74282e-6af1-394b-8d2f-89335dae58de");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> exchange = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
+        String body = exchange.getBody();
+        System.out.println("body = " + body);
+    }
+}
